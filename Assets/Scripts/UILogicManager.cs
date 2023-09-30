afa2497bb885acf5440d799c3e01ace4867227fd
Shizza <64432146+Shizza-cmd@users.noreply.github.com>
2023-09-30 20:06:56 +0300

    UI logic script
    * added:
        Scripts folder and UILogicManager.cs

---
+++ Assets/Scripts/UILogicManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class OnClickPlay : MonoBehaviour
+{
+ [Header("Cameras")]
+ [SerializeField] private Camera UI_MainMenu_Camera;
+ [SerializeField] private Camera Gameplay_Camera;
+
+ [Header("UI")]
+ [SerializeField] private Button Play_Button;
+ [SerializeField] private Canvas UI;
+
+ [Header("Inputs")]
+ [SerializeField] private TMP_Dropdown Dropdown_Menu;
+ [SerializeField] private TMP_InputField Input_Field;
+
+ private void Start()
+ {
+    Button UI_MainMenu_Play = Play_Button.GetComponent<Button>();
+    UI_MainMenu_Play.onClick.AddListener(TaskOnClick);
+ }
+
+ private void Update()
+ {
+
+ }
+
+ void TaskOnClick(){
+    UI_MainMenu_Camera.enabled = false;
+    Gameplay_Camera.enabled = true;
+    
+    UI.enabled = false;
+ }
+}
